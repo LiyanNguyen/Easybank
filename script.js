8a6f65a3ec0c95b16a7dc8880db796d7d4509a7a
LiyanNguyen <liyannguyenn@gmail.com>
2022-05-02 17:24:55 +0800

    Added mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// alert(window.innerHeight);
+let mobileMenuToggle = document.querySelector("#mobileMenuToggle");
+let mobileMenu = document.querySelector("#mobileMenu");
+let isMenuOpen = false;
+
+mobileMenuToggle.onclick = () => {
+	isMenuOpen = !isMenuOpen;
+
+	if (isMenuOpen == true) {
+		mobileMenu.style.display = "block";
+		mobileMenuToggle.src = "images/icon-close.svg";
+	}
+
+	else if (isMenuOpen == false) {
+		mobileMenu.style.display = "none";
+		mobileMenuToggle.src = "images/icon-hamburger.svg";
+	}
+};
